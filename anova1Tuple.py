d26755640837113ec65eb34458bca4895f785ad7
Gustavo Sandri Heidner <sandriheidnerg17@students.ecu.edu>
2017-10-06 17:37:04 -0400

    v0.1

--- anova1Tuple.py
+++ anova1Tuple.py
@@ -5,6 +5,7 @@ Created on Fri Oct  6 14:53:00 2017
 
 """
 from scipy import stats
+import numpy as np
 
 def anova1Tuple(data):
     ''' Computes F-values in a tuple array
@@ -12,19 +13,33 @@ def anova1Tuple(data):
     Output: blahblahblah
     '''
     
-    F, p = stats.f_oneway(data['grp1'], data['grp2'], data['grp3']);
-   
+    k = len(data);
+    n0 = len(data[0])    
+    n1 = len(data[1])
+    N = n0 + n1
+    g0 = np.array(data[0])
+    g1 = np.array(data[1])    
+    
+    
     ''' Degrees of Freedom '''
     dfBetween = k - 1;
     dfWithin = N - k;
     dfTotal = N - 1;
     
     ''' Sum of Squares & Mean Squares'''
-    ssBetween = ((sum(data.groupby('grp3').sum()['grp2']) ** 2) / n) - ((data['grp2'].sum() ** 2) / N);
-    sumYsquared = sum([value ** 2 for value in data['grp2'].values]);
-    ssWithin = sumYsquared - (sum(data.groupby('grp3').sum()['grp2'] ** 2) / n);
-    ssTotal = sumYsquared - ((data['grp2'].sum() ** 2) / N);
-    msBetween = ssBetween / dfBetween;
-    msWithin = ssWithin / dfWithin;
+    X = (sum(g0)+sum(g1))/N    
+    ssTotal = sum((g0 - X)**2) + sum((g1 - X)** 2)
+    ssWithin = sum((g0 - sum(g0)/n0)**2) + sum((g1 - sum(g1)/n1)**2)
+    ssBetween = ssTotal - ssWithin
+    msBetween = ssBetween/dfBetween
+    msWithin = ssWithin / dfWithin
+  
+    ''' F-value, p-value, eta-squared & omega-squared '''  
+    F = msBetween / msWithin
+    F, p = stats.f_oneway(g0, g1) ''' <<-- Gotta test this stuff... '''
+    etaSqrd = ssBetween/ssTotal
+    omegaSqrd = (ssBetween - (dfBetween * msWithin)) / (ssTotal + msWithin)
     
-    F = 
+    return F,p,etaSqrd,omegaSqrd,ssBetween,ssWithin,ssTotal,dfBetween,dfWithin,dfTotal
+ 
+[F,p,etaSqrd,omegaSqrd,ssBetween,ssWithin,ssTotal,dfBetween,dfWithin,dfTotal] = anova1Tuple(data)   
